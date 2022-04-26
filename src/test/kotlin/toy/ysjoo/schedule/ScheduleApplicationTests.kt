de49d1e4f9fad0703f51a94989e5ca03e7c7a5a5
Yongsoo Joo <yonsoo.joo@greenlabs.co.kr>
2022-04-26 10:55:42 +0900

    Initial Commit.

---
+++ src/test/kotlin/toy/ysjoo/schedule/ScheduleApplicationTests.kt
@@ -0,0 +1,13 @@
+package toy.ysjoo.schedule
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ScheduleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
